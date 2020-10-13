e5ce894aea9628c8c1006d801f9f50c92af3c976
Jing Dong <me@jing.io>
2020-10-13 22:00:06 +0100

    add leader key for testing

--- crkbd-custom/config.h
+++ crkbd-custom/config.h
@@ -38,6 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 150
 #define RETRO_TAPPING
 #define IGNORE_MOD_TAP_INTERRUPT
+#define LEADER_TIMEOUT 300
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
--- crkbd-custom/keymap.c
+++ crkbd-custom/keymap.c
@@ -333,6 +333,32 @@ void render_status_secondary(void) {
     render_mod_status_ctrl_shift(get_mods()|get_oneshot_mods());
 }
 
+LEADER_EXTERNS();
+
+void matrix_scan_user(void) {
+  LEADER_DICTIONARY() {
+    leading = false;
+    leader_end();
+
+    SEQ_ONE_KEY(KC_F) {
+      // Anything you can do in a macro.
+      SEND_STRING("QMK is awesome.");
+    }
+    SEQ_TWO_KEYS(KC_D, KC_D) {
+      SEND_STRING(SS_LCTL("a") SS_LCTL("c"));
+    }
+    SEQ_THREE_KEYS(KC_D, KC_D, KC_S) {
+      SEND_STRING("https://start.duckduckgo.com\n");
+    }
+    SEQ_TWO_KEYS(KC_A, KC_S) {
+      register_code(KC_LGUI);
+      register_code(KC_S);
+      unregister_code(KC_S);
+      unregister_code(KC_LGUI);
+    }
+  }
+}
+
 void oled_task_user(void) {
     if (timer_elapsed32(oled_timer) > 30000) {
         oled_off();
